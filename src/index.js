b5543b1bed28b4fde5db2b1135bb706bb50d8698
taoxincn <taoxincn@qq.com>
2024-06-25 09:20:00 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.proxy.taox.in": dockerHub,
+  "quay.proxy.taox.in": "https://quay.io",
+  "gcr.proxy.taox.in": "https://gcr.io",
+  "k8s-gcr.proxy.taox.in": "https://k8s.gcr.io",
+  "k8s.proxy.taox.in": "https://registry.k8s.io",
+  "ghcr.proxy.taox.in": "https://ghcr.io",
+  "cloudsmith.proxy.taox.in": "https://docker.cloudsmith.io",
+  "ecr.proxy.taox.in": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.proxy.taox.in": dockerHub,
 };
 
 function routeByHosts(host) {
